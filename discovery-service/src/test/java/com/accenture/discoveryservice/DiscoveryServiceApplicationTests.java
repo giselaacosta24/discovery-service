9e9d820f56319e227988109c28f6581d829810ce
Gisela Acosta <gacosta1@directvla.com.ar>
2022-09-22 14:08:23 -0300

    agrego Eureka Server

---
+++ discovery-service/src/test/java/com/accenture/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.accenture.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
